2c5850a6f144f85549eb879667df3d17854dec03
Mary Brady <emceebrady@gmail.com>
2019-10-26 10:35:34 -0600

    basic layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,52 @@
 <template>
-  <div class="home">
-    
+  <div class="home container-fluid">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="col-md-10 offset-col-2">
+          <h1 class="borderb">
+            <b>Mary Brady</b>
+          </h1>
+          <p class="text-right mb-0">SEO.</p>
+          <p class="text-right mb-0">Full stack developer.</p>
+          <p class="text-right">Snack enthusiast.</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-12">
+        <hr />
+      </div>
+      <div class="col-lg-4">
+        <i class="fab fa-github"></i>
+      </div>
+      <div class="col-lg-4">
+        <i class="fab fa-linkedin"></i>
+      </div>
+      <div class="col-lg-4">
+        <i class="far fa-folder-open"></i>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-12">
+        <hr />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     HelloWorld
   }
-}
+};
 </script>
+<style class="scoped">
+i {
+  font-size: 10vm;
+}
+</style>
