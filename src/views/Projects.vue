bc62ee51a8d730f6f890f036a8fe222f5a27c870
Mary Brady <emceebrady@gmail.com>
2020-01-11 11:18:03 -0700

    adding projects

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -15,7 +15,7 @@
       </div>
     </div>
     <div class="row h35">
-      <div class="col-md-10 offset-md-1">
+      <div class="col-md-5 offset-md-1">
         <div class="card text-white bg-dark mb-3" style="max-width: 540px;">
           <div class="row no-gutters">
             <div class="col-md-4">
@@ -42,11 +42,195 @@
                     </p>
                   </li>
                 </ul>
-                <a
-                  :href="'http://www.4riversinspections.com' "
-                  class="btn btn-light"
-                  target="_blank"
-                >View</a>
+                <div class="row">
+                  <div class="col-6">
+                    <a
+                      :href="'http://www.4riversinspections.com' "
+                      class="btn btn-light btn-sm"
+                      target="_blank"
+                    >View</a>
+                  </div>
+                  <div class="col-6">
+                    <a
+                      :href="'https://github.com/mary-brady/4riversinspections' "
+                      class="btn btn-light btn-sm"
+                      target="_blank"
+                    >Source</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-5">
+        <div class="card text-white bg-dark mb-3" style="max-width: 540px;">
+          <div class="row no-gutters">
+            <div class="col-md-4">
+              <img src="../assets/YOFT.png" class="card-img img-fluid" alt="..." />
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h3 class="card-title project-border">YOFT</h3>
+                <p
+                  class="card-text"
+                >Boise CodeWorks capstone. Group effort fitness tracker with daily challenges, workout builder, and progress charts.</p>
+                <ul>
+                  <li>
+                    <p class="card-text">
+                      <small>Vuejs</small>
+                    </p>
+                  </li>
+                  <li>
+                    <p class="card-text">
+                      <small>Node.js</small>
+                    </p>
+                  </li>
+                  <li>
+                    <p class="card-text">
+                      <small>ChartJS</small>
+                    </p>
+                  </li>
+                  <li>
+                    <p class="card-text">
+                      <small>Heroku</small>
+                    </p>
+                  </li>
+                </ul>
+                <div class="row">
+                  <div class="col-6">
+                    <a
+                      :href="'https://yoft.herokuapp.com/' "
+                      class="btn btn-light btn-sm"
+                      target="_blank"
+                    >View</a>
+                  </div>
+                  <div class="col-6">
+                    <a
+                      :href="'https://github.com/mary-brady/capstone' "
+                      class="btn btn-light btn-sm"
+                      target="_blank"
+                    >Source</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row h35">
+      <div class="col-md-5 offset-md-1">
+        <div class="card text-white bg-dark mb-3" style="max-width: 540px;">
+          <div class="row no-gutters">
+            <div class="col-md-4">
+              <img src="../assets/Keepr.png" class="card-img" alt="..." />
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h3 class="card-title project-border">Keepr</h3>
+                <p class="card-text">Final Boise CodeWorks project - Pinterest clone.</p>
+                <div class="row">
+                  <div class="col-4">
+                    <ul>
+                      <li>
+                        <p class="card-text">
+                          <small>Vuejs</small>
+                        </p>
+                      </li>
+                      <li>
+                        <p class="card-text">
+                          <small>C#</small>
+                        </p>
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="col-4">
+                    <ul>
+                      <li>
+                        <p class="card-text">
+                          <small>MySQL</small>
+                        </p>
+                      </li>
+                      <li>
+                        <p class="card-text">
+                          <small>Heroku</small>
+                        </p>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-6">
+                    <a
+                      :href="'https://not-pinterest.herokuapp.com/' "
+                      class="btn btn-light btn-sm"
+                      target="_blank"
+                    >View</a>
+                  </div>
+                  <div class="col-6">
+                    <a
+                      :href="'https://github.com/mary-brady/keepr' "
+                      class="btn btn-light btn-sm"
+                      target="_blank"
+                    >Source</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-5">
+        <div class="card text-white bg-dark mb-3" style="max-width: 540px;">
+          <div class="row no-gutters">
+            <div class="col-md-4">
+              <img src="../assets/YOFT.png" class="card-img img-fluid" alt="..." />
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h3 class="card-title project-border">YOFT</h3>
+                <p
+                  class="card-text"
+                >Boise CodeWorks capstone. Group effort fitness tracker with daily challenges, workout builder, and progress charts.</p>
+                <ul>
+                  <li>
+                    <p class="card-text">
+                      <small>Vuejs</small>
+                    </p>
+                  </li>
+                  <li>
+                    <p class="card-text">
+                      <small>Node.js</small>
+                    </p>
+                  </li>
+                  <li>
+                    <p class="card-text">
+                      <small>ChartJS</small>
+                    </p>
+                  </li>
+                  <li>
+                    <p class="card-text">
+                      <small>Heroku</small>
+                    </p>
+                  </li>
+                </ul>
+                <div class="row">
+                  <div class="col-6">
+                    <a
+                      :href="'https://yoft.herokuapp.com/' "
+                      class="btn btn-light btn-sm"
+                      target="_blank"
+                    >View</a>
+                  </div>
+                  <div class="col-6">
+                    <a
+                      :href="'https://github.com/mary-brady/capstone' "
+                      class="btn btn-light btn-sm"
+                      target="_blank"
+                    >Source</a>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -70,5 +254,9 @@ export default {
 <style >
 .projects {
   min-height: 100vh;
+}
+.card-img {
+  width: 100%;
+  height: auto;
 }
 </style>
